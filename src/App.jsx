bf7ad0c169a935570c9061ff935260a4d5dd74cb
Abhishekranjan-01 <abhishekranjan7991@outlook.com>
2024-04-16 16:52:05 +0530

    Authentication Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
 import BackgroundContainer from "./components/BackgroundContainer";
 import AppContainer from "./components/AppContainerItems/AppContainer";
 import Footer from "./components/Footer";
+import SignUp from "./components/Signup/SignUp";
+import { auth } from "./Firebase/Firebase";
+import { onAuthStateChanged } from "firebase/auth";
+import { useEffect, useState } from "react";
+
 function App() {
-  return (
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setIsLoggedIn(true);
+      }
+    });
+  });
+
+  return isLoggedIn ? (
     <>
       <BackgroundContainer />
       <AppContainer />
       <Footer />
     </>
+  ) : (
+    <SignUp />
   );
 }
 
---
+++ src/Firebase/Firebase.jsx
@@ -0,0 +1,64 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { getDatabase } from "firebase/database";
+import { createContext, useContext } from "react";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: "real-time-todo-20643.firebaseapp.com",
+  databaseURL: "https://real-time-todo-20643-default-rtdb.firebaseio.com",
+  projectId: "real-time-todo-20643",
+  storageBucket: "real-time-todo-20643.appspot.com",
+  messagingSenderId: "300774970689",
+  appId: "1:300774970689:web:1d541d6fab99c02a6a3dc0",
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+export const auth = getAuth(firebaseApp);
+const database = getDatabase(firebaseApp);
+
+const FirbaseContext = createContext(null);
+export const useFirebase = () => useContext(FirbaseContext);
+
+export const signUpUserWithEmailAndPassword = async (email, password) => {
+  createUserWithEmailAndPassword(auth, email, password)
+    .then((UserCredential) => {
+      console.log("Signed Up");
+      console.log(UserCredential);
+    })
+    .catch((err) => {
+      console.log("Error");
+      console.log(err);
+    });
+};
+
+export const signInUserWithEmailAndPassword = (email, password) => {
+  signInWithEmailAndPassword(auth, email, password)
+    .then((UserCredential) => {
+      console.log("Signed In");
+      console.log(UserCredential);
+    })
+    .catch((reason) => {
+      console.log(reason);
+    });
+};
+
+export default function FirebaseProvider(props) {
+  return (
+    <FirbaseContext.Provider
+      value={{ signUpUserWithEmailAndPassword, signInUserWithEmailAndPassword }}
+    >
+      {props.children}
+    </FirbaseContext.Provider>
+  );
+}
